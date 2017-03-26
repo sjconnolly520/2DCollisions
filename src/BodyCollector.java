d6e9f3a33bd7506e124fa32a9a1605ce731d06b8
Stephen Connolly <sjconnolly520@gmail.com>
2017-03-25 22:16:53 -0700

    Beging proper structure/design. Really committing so I can save credentials.

--- src/BodyCollector.java
+++ src/BodyCollector.java
@@ -1,16 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BodyCollector {
 	
+	List<MassiveBody> bodies = new ArrayList<>();
+	int numBodies;
+	Scanner scanner = new Scanner(System.in);
+	
+	
 	public BodyCollector(boolean randomize) {
 		
 		System.out.println("How many bodies would you like to simulate?: ");
+		numBodies = scanner.nextInt();
 		
-		Scanner scanner = new Scanner(System.in);
+		if (randomize) {
+			generateRandomBodies();
+		}
+		else {
+			getUserBodyData();
+		}
 		
 		
+	}
+
+	private void getUserBodyData() {
+		// TODO Prompt user for mass, radius, xPos, yPos in a for-loop
 		
-		
+	}
+
+	private void generateRandomBodies() {
+		// TODO Generate specified number of bodies randomly (will need to find adequate/reasonable ranges)
 		
 	}
 
